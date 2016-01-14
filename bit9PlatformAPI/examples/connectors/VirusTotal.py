d618a15bd407dd0fc8b9eefd3748c648ba21e740
Jason Garman <jgarman@bit9.com>
2016-01-14 10:37:17 -0500

    refactor the VT connector in preparation to package it with pyinstaller

--- bit9PlatformAPI/examples/connectors/VirusTotal.py
+++ bit9PlatformAPI/examples/connectors/VirusTotal.py
@@ -48,10 +48,10 @@ import zipfile
 import tempfile
 import shutil
 
-# Import our common bit9api (assumed to live in common folder, sibling to current folder)
-commonPath = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'common')
-sys.path.append(commonPath)
-import bit9api
+import logging
+
+log = logging.getLogger(__name__)
+
 
 # -------------------------------------------------------------------------------------------------
 # VT connector class. Initialization where keys are specified is done at the bottom of the script
@@ -106,7 +106,7 @@ class virusTotalConnector(object):
                 for i in self.bit9.retrieve("v1/pendingAnalysis", url_params="connectorId=" + connectorId):
                     self.processOneAnalysisRequest(i)
             except Exception as e:
-                print("\n*** Exception: %s. Will try again in %d seconds." % (str(e), self.polling_frequency))
+                log.exception("Error during processing. Will try again in %d seconds." % (self.polling_frequency,))
             # Sleep N seconds, and then all over again
             time.sleep(self.polling_frequency)
 
@@ -115,7 +115,7 @@ class virusTotalConnector(object):
         # Unregister our connector
         r = self.bit9.search('v1/connector', ['name:'+self.connector_name])
         if len(r)>0:
-            print("Unregistering connector %s and deleting all its data" % self.connector_name)
+            log.info("Unregistering connector %s and deleting all its data" % self.connector_name)
             self.bit9.delete('v1/connector', r[0])
 
     def uploadFileToVT(self, pa):
@@ -144,6 +144,7 @@ class virusTotalConnector(object):
             pa['analysisError'] = 'Received error when attempting to unzip file from Bit9: %s' % str(e)
             # Update Bit9 status for this file
             self.bit9.update('v1/pendingAnalysis', pa)
+            log.exception("Could not unzip file from Bit9 for analysis of %s" % pa)
             return scanId
 
         outfp.seek(0)
@@ -155,6 +156,7 @@ class virusTotalConnector(object):
             if r.status_code == 200:
                 scanId = r.json()['scan_id']
         except:
+            log.exception("Could not send file %s to VirusTotal" % (pa,))
             isError = True
         finally:
             outfp.close()
@@ -183,16 +185,16 @@ class virusTotalConnector(object):
         positivesPerc = 100 * scanResults.get('positives') / scanResults.get('total')
         if positivesPerc > 50:
             notification['analysisResult'] = 3  # ...malicious
-            notification['severity'] = 'critical';
-            notification['type'] = 'malicious_file';
+            notification['severity'] = 'critical'
+            notification['type'] = 'malicious_file'
         elif positivesPerc > 0:
             notification['analysisResult'] = 2  # ...could be malicious
-            notification['severity'] = 'high';
-            notification['type'] = 'potential_risk_file';
+            notification['severity'] = 'high'
+            notification['type'] = 'potential_risk_file'
         else:
             notification['analysisResult'] = 1  # clean!
-            notification['severity'] = 'low';
-            notification['type'] = 'clean_file';
+            notification['severity'] = 'low'
+            notification['type'] = 'clean_file'
         notification['externalUrl'] = scanResults.get('permalink')
 
         # Enumerate scan results that have detected the issue and build our
@@ -214,7 +216,8 @@ class virusTotalConnector(object):
                 n += 1
         # Send notification
         self.bit9.create("v1/notification", notification)
-        print("VT analysis for fileAnalysis %d completed. VT result is %d%% malware (%s). Reporting status: %s" % (fileAnalysisId, positivesPerc, notification['malwareName'], notification['type']))
+        log.info("VT analysis for fileAnalysis %d completed. VT result is %d%% malware (%s). Reporting status: %s"
+                 % (fileAnalysisId, positivesPerc, notification['malwareName'], notification['type']))
 
     def processOneAnalysisRequest(self, pa):
         # Use md5 hash if we have one. If not, use Sha256
@@ -222,7 +225,6 @@ class virusTotalConnector(object):
         if fileHash == '':
             fileHash = pa['sha256'].strip()
 
-        lastAttempt = None
         scanId = None
         # Check our cache if we already sent this file for scan
         if fileHash in self.scheduledScans.keys():
@@ -240,14 +242,16 @@ class virusTotalConnector(object):
         r.raise_for_status()
         if r.status_code == 204:
             # no results from VT because rate limit was reached. We will try again later.
+            log.info("VirusTotal API rate limit reached, will try again later")
             return
 
+        scanId = None
         scanResults = r.json()
         # Check if we got results...
         if scanResults.get('positives') is not None:
             # Yes, VT has them. Report results and we are done with the file
+            log.info("%s: VirusTotal has a result ready, reporting to Bit9" % fileHash)
             self.reportResultToBit9(pa['id'], scanResults)
-            scanId =  None
         elif scanResults.get('scan_id') is not None:
             # VT already knows about the file, but scan is not complete. We got scanId for future reference
             # Let's remember that and try again in 1 hour (per VT best practices)
@@ -255,40 +259,54 @@ class virusTotalConnector(object):
         elif not self.allow_uploads:
             # Uploads are not allowed. Cancel the analysis
             pa['analysisStatus'] = 5 # (status: Cancelled)
+            log.info("%s: VirusTotal has no information and we aren't allowed to upload it. Cancelling the analysis request.")
             self.bit9.update('v1/pendingAnalysis', pa)
         elif pa['uploaded'] == 1:
             # We have file and now we will upload it to VT
+            log.info("%s: VirusTotal has no information on this hash. Uploading the file")
             scanId = self.uploadFileToVT(pa)
         else:
             # if we end here, it means that VT doesn't have file, and Bit9 hasn't uploaded it yet from the agent
             # we will come back again when we reach this file next time around
-            scanId = None
+            log.info("%s: VirusTotal has no information on this hash. Waiting for Bit9 agent to upload it.")
+            pass
 
-        if scanId is not None:
+        if scanId:
             # Remember scanId since VT wants use to use it for future references to the file
             # We will try again in 1 hour (per VT best practices)
-            self.scheduledScans[fileHash] = {'scanId': scanId, 'nextCheck': datetime.datetime.now()
-                                                                      + datetime.timedelta(0, 3600)}
+            next_check = datetime.datetime.now() + datetime.timedelta(0, 3600)
+            self.scheduledScans[fileHash] = {'scanId': scanId, 'nextCheck': next_check}
+            log.info("%s: Waiting for analysis to complete. Will check back after %s." % (fileHash,
+                                                                                          next_check.strftime("%Y-%m-%d %H:%M:%S")))
 
-# -------------------------------------------------------------------------------------------------
-# Main body of the script
+if __name__ == '__main__':
+    # -------------------------------------------------------------------------------------------------
+    # Main body of the script
 
-bit9 = bit9api.bit9Api(
-    "https://localhost",  # Replace with actual Bit9 server URL
-    token="<enter your Bit9 API token here>",  # Replace with actual Bit9 user token for VT integration
-    ssl_verify=False  # Don't validate server's SSL certificate. Set to True unless using self-signed cert on IIS
-)
+    try:
+        import bit9api
+    except ImportError:
+        # Import our common bit9api (assumed to live in common folder, sibling to current folder)
+        commonPath = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'common')
+        sys.path.append(commonPath)
+        import bit9api
 
-vtConnector = virusTotalConnector(
-    bit9,
-    vt_token='<enter your VT API key here>',  # Replace with your VT key
-    allow_uploads=True,  # Allow VT connector to upload binary files to VirusTotal
-    connector_name='VirusTotal',
-    download_location=r'c:\test'  # Replace with actual local file location. If not set,
-                                  # script will try to access shared folder where this file resides
-                                  # Note that you do not want to end your path with a backslash. ie. use
-                                  # r'c:\test' *not* r'c:\test\'.
-)
+    bit9 = bit9api.bit9Api(
+        "https://localhost",  # Replace with actual Bit9 server URL
+        token="<enter your Bit9 API token here>",  # Replace with actual Bit9 user token for VT integration
+        ssl_verify=False  # Don't validate server's SSL certificate. Set to True unless using self-signed cert on IIS
+    )
 
-print("\n*** VT script starting")
-vtConnector.start()
+    vtConnector = virusTotalConnector(
+        bit9,
+        vt_token='<enter your VT API key here>',  # Replace with your VT key
+        allow_uploads=True,  # Allow VT connector to upload binary files to VirusTotal
+        connector_name='VirusTotal',
+        download_location=r'c:\test'  # Replace with actual local file location. If not set,
+                                      # script will try to access shared folder where this file resides
+                                      # Note that you do not want to end your path with a backslash. ie. use
+                                      # r'c:\test' *not* r'c:\test\'.
+    )
+
+    print("\n*** VT script starting")
+    vtConnector.start()
