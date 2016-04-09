0a50ec3f1e676c27c5181bf5d0945f6812e7e3f0
BJ Swope <bigblueswope+github+public@gmail.com>
2016-04-09 12:46:03 -0400

    changed api_key to api_token and added list of computer fields to end of script for reference.

--- bit9PlatformAPI/examples/bit9_list_of_comps_and_enf_levels.py
+++ bit9PlatformAPI/examples/bit9_list_of_comps_and_enf_levels.py
@@ -10,9 +10,9 @@ userhome = os.path.expanduser('~')
 csv_name = userhome + '/Desktop/test.csv'
 
 server='https://bit9server.bit9se.com/'
-api_key='AACB5C5F-D9B4-4694-AB9A-8640FF79D401'
+api_token= 'AACB5C5F-D9B4-4694-AB9A-8640FF79D401'
 
-bit9 = bit9Api (server, token=api_key, ssl_verify=False)
+bit9 = bit9Api (server, token=api_token, ssl_verify=False)
 search_conditions = ['']
 #search_conditions = ['uninstalled:False']
 
@@ -38,3 +38,76 @@ for c in comps:
     a.writerow([c['name'], enf_dict[c['enforcementLevel']], c['ipAddress'], c['uninstalled'], c['policyName'],c['lastPollDate']])
     print c['name'], enf_dict[c['enforcementLevel']], c['ipAddress'], c['uninstalled'], c['policyName'], c['lastPollDate']
 b.close()
+
+
+'''Available computer fields as of Bit9 7.2.1.710
+CLIPassword
+SCEPStatus
+agentCacheSize
+agentMemoryDumps
+agentQueueSize
+agentVersion
+automaticPolicy
+cbSensorFlags
+cbSensorId
+cbSensorVersion
+ccFlags
+ccLevel
+clVersion
+computerTag
+connected
+dateCreated
+daysOffline
+debugDuration
+debugFlags
+debugLevel
+deleted
+description
+disconnectedEnforcementLevel
+enforcementLevel
+forceUpgrade
+hasHealthCheckErrors
+id
+initializing
+ipAddress
+kernelDebugLevel
+lastPollDate
+lastRegisterDate
+localApproval
+macAddress
+machineModel
+memorySize
+name
+osName
+osShortName
+platformId
+policyId
+policyName
+policyStatusDetails
+prioritized
+processorCount
+processorModel
+processorSpeed
+refreshFlags
+supportedKernel
+syncFlags
+syncPercent
+systemMemoryDumps
+tamperProtectionActive
+tdCount
+template
+templateCloneCleanupMode
+templateCloneCleanupTime
+templateCloneCleanupTimeScale
+templateComputerId
+templateDate
+templateTrackModsOnly
+uninstalled
+upgradeError
+upgradeErrorCount
+upgradeErrorTime
+upgradeStatus
+users
+virtualPlatform
+virtualized
+'''
